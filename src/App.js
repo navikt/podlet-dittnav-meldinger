4b23bf371e87642bdff08358574f5d7e50d9cbf6
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-01-27 19:03:58 +0100

    La til dittnav-lenkepanel

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import InfoMeldinger from "./components/Infomeldinger";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./components/scroll/ScrollToTop";
 import Varslinger from "./pages/Varslinger/Varslinger";
+import DittnavLenkePanel from "./components/DittnavLenkePanel";
 
 const App = () => {
   return (
     <div className="podlet-dittnav-meldinger">
       <InfoMeldinger />
+      <DittnavLenkePanel />
       <Router basename="/person">
         <ScrollToTop />
         <Switch>
