9239500a4ffbabdf1ddf085f8d2185d950b7cf9a
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-02-13 14:39:22 +0100

    Raskere fetch av data fra dittnav-api

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./components/scroll/ScrollToTop";
 import Varslinger from "./pages/Varslinger/Varslinger";
 import DittnavLenkePanel from "./components/DittnavLenkePanel";
+import useBrukernotifikasjoner from "./hooks/useBrukernotifikasjoner";
+import useInaktiveBrukernotifikasjoner from "./hooks/useInaktiveBrukernotifikasjoner";
+import usePerson from "./hooks/usePerson";
+import useSaker from "./hooks/useSaker";
+import useInnloggingsstatus from "./hooks/useInnloggingsstatus";
 
 const App = () => {
+  useBrukernotifikasjoner();
+  useInaktiveBrukernotifikasjoner();
+  usePerson();
+  useSaker();
+  useInnloggingsstatus();
+
   return (
     <div className="podlet-dittnav-meldinger">
       <InfoMeldinger />
